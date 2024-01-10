c8abdfffde82a80d892e51b97d9c1046c507dc64
Jaime <Jaime@PCJaime>
2024-01-10 10:40:09 +0100

    Commmit: Añadir p2.java y pom.xml

---
+++ P2/src/main/java/com/myproject/p2/P2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myproject.p2;
+
+/**
+ *
+ * @author Jaime
+ */
+public class P2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hijo puta");
+    }
+}
